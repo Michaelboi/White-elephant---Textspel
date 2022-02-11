36e721d5be25342cdd90ce2fd64cbfb104336674
Michaelboi <95038340+Michaelboi@users.noreply.github.com>
2022-02-11 10:39:06 +0100

    Add files via upload

--- Grupp uppgift- textspel.py
+++ Grupp uppgift- textspel.py
@@ -2,11 +2,20 @@ import openpyxl
 from openpyxl import Workbook, load_workbook
 from openpyxl.utils import get_column_letter
 
-wb = load_workbook('C:/Users/MichaelNwaijah/Documents/Textspel.xlsx')
+# Laddar in spreadsheet
+wb = load_workbook('G:/Min enhet/1Textspel.xlsx')
 ws = wb.active
-for row in range(1, 4):
-    for col in range(1, 4):
-        char = get_column_letter(col)
-        print(ws[char + str(row)].value)
 
-wb.save('C:/Users/MichaelNwaijah/Documents/Textspel.xlsx')
+Start = 'B2'
+player = Start
+
+while player != 'B8':
+    def player():
+
+
+#for row in range(2, 5):
+    #for col in range(1, 5):
+        #char = get_column_letter(col)
+        #print(ws[char + str(row)].value)
+
+wb.save('/1Textspel.xlsx')
