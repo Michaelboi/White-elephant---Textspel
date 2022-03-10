cdf86bec4ac197cbfef523bd328ce7b79035766a
Michaelboi <95038340+Michaelboi@users.noreply.github.com>
2022-03-10 11:16:15 +0100

    Add files via upload

--- Grupp uppgift- textspel.py
+++ Grupp uppgift- textspel.py
@@ -19,45 +19,50 @@ room4 = ws['C5'].value
 room5 = ws['C6'].value
 room6 = ws['C7'].value
 Exit = ws['B10'].value
-current_position = [room1, room2, room3, room4, room5, room6]
+current_pos = [room1, room2, room3, room4, room5, room6]
 directions = ['north', 'south', 'east', 'west']
+current_room = 2
 
-def current_position():
-    print(current_position[0])
-
-
-
-
+# kollar nuvarande postion
 def look():
-    print(current_position[0, 6])
+    print(ws[f"C{current_room}"].value)
 
+
+# förflytar spelaren
 def north():
-    print()
+    if current_room == 2
+
 
 def south():
     print()
 
+
 def east():
     print()
+
 
 def west():
     print()
 
-while current_position != ws['B8'].value:
-    playerInput = str(input(''))
-    if 'look' in playerInput:
-        look()
-    if 'north' in playerInput:
-        north()
 
-current_position = room1
-
-
-
-#for row in range(2, 5):
-    #for col in range(1, 5):
-        #char = get_column_letter(col)
-        #print(ws[char + str(row)].value)
+# En loop som håller kvar spelaren i input
+def textspel():
+    while current_pos != ws['B8'].value:
+        player_input = str(input(''))
+        if 'look' in player_input:
+            look()
+        if 'north' in player_input:
+            north()
+        if 'south' in player_input:
+            south()
+        if 'west' in player_input:
+            west()
+        if 'east' in player_input:
+            east()
+textspel()
+# for row in range(2, 5):
+# for col in range(1, 5):
+# char = get_column_letter(col)
+# print(ws[char + str(row)].value)
 
 wb.save('C:/Users/MichaelNwaijah/Downloads/3Textspel.xlsx')
-
