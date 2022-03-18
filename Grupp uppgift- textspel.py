a724697acddd126cc69813af1f6fd28f9a132b5c
Michaelboi <95038340+Michaelboi@users.noreply.github.com>
2022-03-18 15:15:51 +0100

    Add files via upload

--- Grupp uppgift- textspel.py
+++ Grupp uppgift- textspel.py
@@ -1,59 +1,61 @@
 import openpyxl
 from openpyxl import Workbook, load_workbook
-import openpyxl.utils
-from openpyxl.utils import get_column_letter
 
-# Laddar in spreadsheet
+
+# Laddar in Excel-filen och konverterar det till en variabel
 wb = load_workbook('C:/Users/MichaelNwaijah/Downloads/Textspel1.xlsx')
 ws = wb.active
 
-# Worksheets
+# Använder variabeln för Excel-filen och tar namnen av sheeten i filen för att konvertera även de till variabler
 wsRooms = wb['Rooms']
 wsMap = wb['Map']
 wsSave = wb['Save']
 
-#välkommnar spelaren och ger instruktioner på hur man går framåt, bakåt, höger, vänster
+# Välkommnar spelaren och berättar vad move-kommandona är; framåt, bakåt, höger, vänster
 print("\nWelcome to our game, hope you enjoy it")
 print("The Move Commands are: North, South, West, East")
 
-# start värden till start punkten
-current_room_Y = 1
-current_room_X = 1
-# Ändrings värden till X och Y
+# Definierar de två variablerna för de två koordinaterna, de två som står där från början är startkoordinaterna
+current_room_Y = 2
+current_room_X = 2
+# Variabel för hur mycket ett koordinatvärde förändras. Även en dictionary där Excels kolumnkoordinater omvandlas till siffror för att göra det enklare
 current_room_change = 1
-X_positions = {1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E'}
+X_positions = {1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E', 6: 'F', 7: 'G'}
+# Variabeln som berättar om while-loopen för spelet är av eller på
 start = True
 
 
-# Kollar nuvarande postion
+# Definierar kommandot look, printar värdet i cellen där man befinner sig, alltså ett rum ID
 def look():
     print(wsMap[f"{X_positions[current_room_X]}{current_room_Y}"].value)
 
 
-
-# förflytar spelaren
-# Ner
+# De fyra olika move kommandona för de fyra olika hållen man kan gå
+# Syd- och norrkommandona tar variabeln för y koordinaten och adderar eller subtraherar 1 från värdet för att få det nya värdet, printar sedan ID av rummet du flyttat till, alltså som look
+# Norr och söder är tvärtom vad gäller + och - på grund av att man går ner i excel om värdet på y blir större
+# Söder
 def south():
     global current_room_Y
     if current_room_Y >= 1:
         current_room_Y += current_room_change
         print(wsMap[f'{X_positions[current_room_X]}{current_room_Y}'].value)
 
-# Upp
+# Norr
 def north():
     global current_room_Y
     if current_room_Y >= 1:
         current_room_Y -= current_room_change
         print(wsMap[f'{X_positions[current_room_X]}{current_room_Y}'].value)
 
-# Höger
+# Öst- och västkommandona tar variabeln för x koordinaten och adderar eller subtraherar 1 från värdet för att få det nya värdet, printar sedan ID av rummet du flyttat till, alltså som look
+# Öst
 def east():
     global current_room_X
     if current_room_X >= 1:
         current_room_X += current_room_change
         print(wsMap[f'{X_positions[current_room_X]}{current_room_Y}'].value)
 
-# Vänster
+# Väst
 def west():
     global current_room_X
     if current_room_X >= 1:
@@ -61,7 +63,7 @@ def west():
         print(wsMap[f'{X_positions[current_room_X]}{current_room_Y}'].value)
 
 
-# En loop som håller kvar spelaren i input med ett stop villkor
+# En while-loop som håller kvar spelaren i input med ett stop villkor, utför ett av de 5 kommandona beroende på vad spelaren skriver, bryter loopen om spelaren skriver 'quit'
 while start:
     player_input = str(input(''))
     if 'quit' in player_input:
@@ -76,24 +78,31 @@ while start:
         west()
     if player_input == 'east':
         east()
-# Exit
-    if player_input == 'B4':
+# Berättar för spelaren att de vunnit och bryter loopen om de kommer till koordinaterna för slutet
+    if current_room_X == 3 and current_room_Y == 5:
+        print('\nYou win!')
         start = False
 
-# Vägar i spelet
-    if current_room_Y == 2 and current_room_X == 5:
-        current_room_X = 4
-    if current_room_X == 5 and current_room_Y == 3:
-        current_room_X = 4
-    if current_room_X == 5 and current_room_Y == 1:
-        current_room_X = 4
-    if current_room_Y == 4 and current_room_X == 3:
-        current_room_Y = 3
-    if current_room_Y == 4 and current_room_X == 4:
-        current_room_Y = 3
-    if current_room_X == 1 and current_room_Y == 3:
+
+# Väggar i spelet
+# Puttar tillbaka spelaren in i mappen om de försöker komma utanför den genom att sätta koordinaterna tillbaka till det de var innan spelaren försökte komma utanför
+    if current_room_X == 1:
+        current_room_X = 2
+    if current_room_Y == 1:
         current_room_Y = 2
-    if current_room_X == 4 and current_room_Y == 1:
-        current_room_X = 3
+    if current_room_Y == 2 and current_room_X == 6:
+        current_room_X = 5
+    if current_room_X == 6 and current_room_Y == 3:
+        current_room_X = 5
+    if current_room_X == 6 and current_room_Y == 2:
+        current_room_X = 5
+    if current_room_Y == 5 and current_room_X == 4:
+        current_room_Y = 4
+    if current_room_Y == 5 and current_room_X == 5:
+        current_room_Y = 4
+    if current_room_X == 2 and current_room_Y == 4:
+        current_room_Y = 3
+    if current_room_X == 5 and current_room_Y == 2:
+        current_room_X = 4
 
 wb.save('C:/Users/MichaelNwaijah/Downloads/Textspel1.xlsx')
